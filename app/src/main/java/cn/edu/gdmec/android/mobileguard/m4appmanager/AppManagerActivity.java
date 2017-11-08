f7e1f8d7a96411aebc6899d4603562e25dd99c81
luoruifeng <1462778722@qq.com>
2017-11-08 15:39:21 +0800

    2017.11.08 第四章 软件管家

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m4appmanager/AppManagerActivity.java
@@ -0,0 +1,8 @@
+package cn.edu.gdmec.android.mobileguard.m4appmanager;
+
+/**
+ * Created by student on 17/11/8.
+ */
+
+public class AppManagerActivity {
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m4appmanager/entity/Applnfo.java
@@ -0,0 +1,8 @@
+package cn.edu.gdmec.android.mobileguard.m4appmanager.entity;
+
+/**
+ * Created by student on 17/11/8.
+ */
+
+public class Applnfo {
+}
