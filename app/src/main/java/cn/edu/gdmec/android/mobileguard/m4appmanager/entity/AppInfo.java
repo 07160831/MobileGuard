3378e07fe889ec1b285010eb5c45b3d007e9ae15
07160831 <484266255@qq.com>
2017-11-12 20:36:20 +0800

    软件管家

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m4appmanager/entity/AppInfo.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m4appmanager/entity/AppInfo.java
@@ -3,32 +3,37 @@ package cn.edu.gdmec.android.mobileguard.m4appmanager.entity;
 import android.graphics.drawable.Drawable;
 
 /**
- * Created by student on 17/11/8.
+ * Created by Administrator on 2017/11/5.
  */
 
 public class AppInfo {
-   /* 应用程序包名*/
-    public String packgeName;
+    public String version;
+    public String installTime;
+    public String certifi;
+    public String permisstion;
+    /*应用程序包名*/
+    public String packageName;
     /*应用程序图标*/
     public Drawable icon;
     /*应用程序名称*/
     public String appName;
     /*应用程序路径*/
-    private String appPath;
+    public String apkPath;
     /*应用程序大小*/
     public long appSize;
     /*是否是手机存储*/
     public boolean isInRoom;
     /*是否是用户应用*/
-    public boolean isUsreApp;
-    /*是否选中，默认都为false*/
+    public boolean isUserApp;
+    /*是否选中 默认是false*/
     public boolean isSelected = false;
-    /*拿到app位置字符串*/
-    public String getApppLocation(boolean isInRoom){
+/*拿到App位置字符串/
+ */
+    public String getAppLocation(boolean isInRoom){
         if (isInRoom){
             return "手机内存";
         }else {
-            return "外部内存";
+            return "外部存储";
         }
     }
 }
