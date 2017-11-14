bd4496cf347f9f1fcb354aa23131d36c5e3e158b
luoruifeng <1462778722@qq.com>
2017-11-14 11:36:30 +0800

    2017.11.14  活动按钮添加

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m4appmanager/adapter/AppManagerAdapter.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m4appmanager/adapter/AppManagerAdapter.java
@@ -90,6 +90,7 @@ public class AppManagerAdapter extends BaseAdapter {
         }else {
             viewHolder = new ViewHolder();
             view =View.inflate(context,R.layout.item_appmanager_list,null);
+            viewHolder.mAbouticon1TV = (TextView) view.findViewById(R.id.tv_abouticon1_app);
             viewHolder.mAbouticonTV = (TextView) view.findViewById(R.id.tv_abouticon_app);
             viewHolder.mAppIconImgv = (ImageView) view.findViewById(R.id.imgv_appicon);
             viewHolder.mAppLocationTV = (TextView) view.findViewById(R.id.tv_appisroom);
@@ -120,6 +121,7 @@ public class AppManagerAdapter extends BaseAdapter {
             viewHolder.mShareAppTV.setOnClickListener(listener);
             viewHolder.mUninstallTV.setOnClickListener(listener);
             viewHolder.mAbouticonTV.setOnClickListener(listener);
+            viewHolder.mAbouticon1TV.setOnClickListener(listener);
         }
         return view;
     }
@@ -135,6 +137,7 @@ public class AppManagerAdapter extends BaseAdapter {
         return tv;
     }
     static class ViewHolder {
+        TextView mAbouticon1TV;
         TextView mAbouticonTV;
         TextView mLuanchAppTV;
         TextView mUninstallTV;
@@ -174,6 +177,9 @@ public class AppManagerAdapter extends BaseAdapter {
                     break;
                 case R.id.tv_abouticon_app:
                     EngineUtils.AbouticonAppDetail(context,appInfo);
+                    break;
+                case R.id.tv_abouticon1_app:
+                    EngineUtils.ActivityInfo(context,appInfo);
                     break;
             }
         }
